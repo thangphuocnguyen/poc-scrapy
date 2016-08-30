5ab4628c98ead62c4a6a3bd377b2bb3247c180a7
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-08-30 14:11:03 +0700

    Update specs for ghost_spider to log the link from LinkExtractor

--- scrapy-demo/scrapydemo/scrapydemo/items.py
+++ scrapy-demo/scrapydemo/scrapydemo/items.py
@@ -8,7 +8,20 @@
 import scrapy
 
 class GhostLinkItem(scrapy.Item):
-    link = scrapy.Field()
+
+    # session_id: a unique session id for each scrapy run or harvest
+    session_id = scrapy.Field()
+
+    # depth: the depth of the current page with respect to the start url
+    depth = scrapy.Field()
+
+    # current_url: the url of the current page being processed
+    current_url = scrapy.Field()
+
+    # referring_url: the url of the site which was linked to the current page
+    referring_url = scrapy.Field()
+
+    title = scrapy.Field()
 
 
 class GhostPostItem(scrapy.Item):
--- scrapy-demo/scrapydemo/scrapydemo/spiders/ghost_spider.py
+++ scrapy-demo/scrapydemo/scrapydemo/spiders/ghost_spider.py
@@ -1,23 +1,44 @@
 
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
 from scrapydemo.items import GhostLinkItem
+
 
 class GhostSpider(CrawlSpider):
 
     name = 'ghost_spider'
-    allowed_domains = ["192.168.99.101"]
+    allowed_domains = ['192.168.99.101']
+
+    session_id = -1
 
     start_urls = [
-        "http://192.168.99.101/",
+        'http://192.168.99.101/',
     ]
 
-    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse_obj', follow=True),)
+    rules = (Rule(LinkExtractor(allow=()),
+             callback='parse_item',
+             follow=True),)
 
-    def parse_obj(self,response):
+    # receive session_id arguments in constructors, default = -1
+    # specs session_id in command line by using -a session_id=<id>
+    # ex: $ scrapy crawl ghost_spider -a session_id=1
+    def __init__(self, session_id=-1, *args, **kwargs):
+        super(GhostSpider, self).__init__(*args, **kwargs)
+        self.session_id = session_id
+
+    def parse_item(self, response):
 
         item = GhostLinkItem()
-        item['link'] = []
-        for link in LxmlLinkExtractor(allow=(), deny=self.allowed_domains).extract_links(response):
-            item['link'].append(link.url)
-        yield item
+
+        item['session_id'] = self.session_id
+        item['current_url'] = response.url
+        item['title'] = response.xpath('//title/text()').extract()
+
+        item['depth'] = response.meta['depth']
+
+        # response.request.headers.get('Referer', None) come from
+        # the RefererMiddleware which is defaulted by SPIDER_MIDDLEWARES_BASE
+        item['referring_url'] = response.request.headers.get('Referer', None) \
+                                        .decode('utf-8')
+
+        return item
