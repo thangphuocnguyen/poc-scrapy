8fc8227a172cbe8f4801d40a062b901ed193ded9
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-09-05 16:50:16 +0700

    Add log into parse method

--- scrapy-demo/scrapydemo/scrapydemo/spiders/ghost_spider.py
+++ scrapy-demo/scrapydemo/scrapydemo/spiders/ghost_spider.py
@@ -8,12 +8,12 @@ class GhostSpider(CrawlSpider):
     """GhostSpider."""
 
     name = 'ghost_spider'
-    allowed_domains = ['192.168.99.101', 'localhost:2368']
+    allowed_domains = ['192.168.99.100', 'localhost:2368']
 
     session_id = -1  # Default session_id
 
     start_urls = [
-        'http://192.168.99.101/',
+        'http://192.168.99.100/',
     ]
 
     rules = (
@@ -40,34 +40,53 @@ class GhostSpider(CrawlSpider):
 
     def parse_item(self, response):
         """Parse item."""
-        print('hello--------------------')
+        #
+
         item = GhostLinkItem()
+        self.log('KIND -----> Ghost link!\n===========================')
 
         item['session_id'] = self.session_id
+        self.log('session_id: %s' % item['session_id'])
+
         item['current_url'] = response.url
+        self.log('current_url: %s' % item['current_url'])
+
         item['title'] = response.xpath('//title/text()').extract()
+        self.log('title: %s' % item['title'])
 
         item['depth'] = response.meta['depth']
+        self.log('depth: %s' % item['depth'])
 
         # response.request.headers.get('Referer', None) come from
         # the RefererMiddleware which is defaulted by SPIDER_MIDDLEWARES_BASE
         item['referring_url'] = response.request.headers.get('Referer', None) \
                                         .decode('utf-8')
+        self.log('referring_url: %s' % item['referring_url'])
 
         yield item
 
     def parse_author_posts(self, response):
         """Parse post from author's page."""
         #
-        print('res+++++++++++++++++++++++')
+
         for sel in response.xpath("//article[@class='post']"):
 
             post_item = GhostPostItem()
-            post_item['title'] = sel.xpath('header/h2/a/text()').extract_first()
+            self.log('KIND -----> Ghost post!\n===========================')
+
+            post_item['title'] = sel.xpath('header/h2/a/text()') \
+                                    .extract_first()
+            self.log('title: %s' % post_item['title'])
+
             post_item['link'] = sel.xpath('header/h2/a/@href').extract_first()
+            self.log('link: %s' % post_item['link'])
+
             post_item['desc'] = sel.xpath('section/p/text()').extract_first()
+            self.log('desc: %s' % post_item['desc'])
+
             post_item['author'] = sel.xpath('footer/a/text()').extract_first()
-            # post_item['author'] = sel.xpath('section/p/text()').extract_first()
+            self.log('author: %s' % post_item['author'])
+
             yield post_item
 
         ghost_link_item = self.parse_item(response)
